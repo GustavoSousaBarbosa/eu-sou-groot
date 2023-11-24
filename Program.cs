413a3040f0a11515455b902a9773c69739916889
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-24 20:31:17 -0300

    atividade eu sou groot

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿const string FraseParaSair = "tchau";
+
+Console.WriteLine("---- Groot chat -----\n");
+
+string pergunta;
+do
+{
+    Console.WriteLine("pergunta: ");
+    pergunta = Console.ReadLine()!;
+    string resposta = obtemrespostagroot(pergunta);
+    exiberesposta(obtemrespostagroot(pergunta));
+
+} while (pergunta.Trim().ToLower() != FraseParaSair);
+
+void exiberesposta(string resposta)
+
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($" Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+string obtemrespostagroot(string pergunta)
+{
+    return pergunta.Trim().ToLower()== FraseParaSair ?
+    "Eu sou Groot!" :
+    "Eu sou Groot.";
+}
